f6fd93bdd9ec80ee43e88d3407be04ebd22a303d
brunomartins-com <hello@brunomartins.com>
2016-08-16 09:14:58 -0400

    First commit with Lumen files

---
+++ database/migrations/2016_08_16_121641_create_contact_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('contactId');
+            $table->string('name', 100);
+            $table->string('phone', 14)->unique();
+            $table->integer('company');
+            $table->timestamps();
+            $table->foreign('company')->references('companyId')->on('company')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('contact')) {
+            Schema::drop('contact', function (Blueprint $table) {
+                $table->dropForeign('contact_company_foreign');
+            });
+        }
+    }
+}
---
+++ database/migrations/2016_08_16_122211_create_company_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->increments('companyId');
+            $table->string('name', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('company');
+    }
+}
